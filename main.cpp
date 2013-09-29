f6f060ceb02f4851f7e2617d9cd1ab8d3f6b7b45
carl <carl@soldersplash.co.uk>
2013-09-29 22:02:29 +0100

    Fixed linker issue, mbed are using a dynamic vector table in the first section of ram

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include "mbed.h"
 
-DigitalOut myled(LED1);
+DigitalOut myled(p40);
 
 int main()
 {
+	// TODO : This should be called in the startup file by the toolchain
 	SystemInit();
 
     while(1)
